69b9b31690e38ca7d4e6d4665aa5da95e35211bb
christianeriksson06 <christianeriksson26@gmail.com>
2025-09-01 14:21:26 +0200

    Färdig med array 4

---
+++ array4.js
@@ -0,0 +1,21 @@
+/*const personer = [
+  { fornamn: "Anna", efternamn: "Andersson", stad: "Stockholm" },
+  { fornamn: "Erik", efternamn: "Eriksson", stad: "Göteborg" },
+  { fornamn: "Sara", efternamn: "Svensson", stad: "Malmö" },
+  { fornamn: "Lars", efternamn: "Larsson", stad: "Stockholm" },
+  { fornamn: "Emma", efternamn: "Ek", stad: "Göteborg" },
+  { fornamn: "Nils", efternamn: "Nilsson", stad: "Malmö" },
+  { fornamn: "Lisa", efternamn: "Lindberg", stad: "Stockholm" },
+  { fornamn: "Johan", efternamn: "Johansson", stad: "Göteborg" },
+  { fornamn: "Maria", efternamn: "Månsson", stad: "Malmö" },
+  { fornamn: "Per", efternamn: "Persson", stad: "Stockholm" }
+];
+
+const personCity = personer.reduce((räkna, personer) => {
+    räkna[personer.stad] = (räkna[personer.stad] || 0) + 1;
+    return räkna;
+}, {});
+
+console.log("In-värde: Array med alla 10 personer:", personer);
+console.log("Ut-värde: Objekt", personCity);
+*/
