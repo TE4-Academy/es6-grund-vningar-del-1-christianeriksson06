2fa193e2e75f99ae57d5890e433d70a0b8aafd4e
christianeriksson06 <christianeriksson26@gmail.com>
2025-09-01 12:30:41 +0200

    Färdig med array 1

---
+++ array1.js
@@ -0,0 +1,15 @@
+const personer = [
+  { fornamn: "Anna", efternamn: "Andersson", stad: "Stockholm" },
+  { fornamn: "Erik", efternamn: "Eriksson", stad: "Göteborg" },
+  { fornamn: "Sara", efternamn: "Svensson", stad: "Malmö" },
+  { fornamn: "Lars", efternamn: "Larsson", stad: "Stockholm" },
+  { fornamn: "Emma", efternamn: "Ek", stad: "Göteborg" },
+  { fornamn: "Nils", efternamn: "Nilsson", stad: "Malmö" },
+  { fornamn: "Lisa", efternamn: "Lindberg", stad: "Stockholm" },
+  { fornamn: "Johan", efternamn: "Johansson", stad: "Göteborg" },
+  { fornamn: "Maria", efternamn: "Månsson", stad: "Malmö" },
+  { fornamn: "Per", efternamn: "Persson", stad: "Stockholm" }
+];
+const stockholmFilter = personer.filter(person => person.stad === "Stockholm");
+console.log("In-värde: Array med alla 10 personer:", personer);
+console.log("Ut-värde: Array med 4 personer från stockholm", stockholmFilter);
