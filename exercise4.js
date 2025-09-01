7fd99dd580d5c086272886641f4cd39c692e46f2
christianeriksson06 <christianeriksson26@gmail.com>
2025-09-01 10:44:33 +0200

    Färdig med exercise 4

---
+++ exercise4.js
@@ -0,0 +1,5 @@
+const names = ["anna", "erik", "sara"];
+const upperNames = [];
+const upperNamesMap = names.map(name => name.toUpperCase());
+console.log ("In-värde: Array:", names);
+console.log("Ut-värde: Array:", upperNamesMap);
