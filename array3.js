6eae3af092c8c4b1dde3ed75110f27573b24cfc7
christianeriksson06 <christianeriksson26@gmail.com>
2025-09-01 12:44:47 +0200

    Färdig med array 3

---
+++ array3.js
@@ -0,0 +1,16 @@
+const personer = [
+  { fornamn: "Anna", efternamn: "Andersson", stad: "Stockholm" },
+  { fornamn: "Erik", efternamn: "Eriksson", stad: "Göteborg" },
+  { fornamn: "Sara", efternamn: "Svensson", stad: "Malmö" },
+  { fornamn: "Lars", efternamn: "Larsson", stad: "Stockholm" },
+  { fornamn: "Emma", efternamn: "Ek", stad: "Göteborg" },
+  { fornamn: "Nils", efternamn: "Nilsson", stad: "Malmö" },
+  { fornamn: "Lisa", efternamn: "Lindberg", stad: "Stockholm" },
+  { fornamn: "Johan", efternamn: "Johansson", stad: "Göteborg" },
+  { fornamn: "Maria", efternamn: "Månsson", stad: "Malmö" },
+  { fornamn: "Per", efternamn: "Persson", stad: "Stockholm" }
+];
+
+const findName = personer.find(person => person.efternamn === "Svensson")
+console.log("In-värde: Array med alla 10 personer:", personer);
+console.log("Ut-värde: Ett objekt", findName);
