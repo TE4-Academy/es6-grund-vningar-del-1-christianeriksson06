8d6778f6efb1cf567d08dbadce23c87c6396776c
christianeriksson06 <christianeriksson26@gmail.com>
2025-09-01 14:21:58 +0200

    Färdig med array 5

---
+++ array5.js
@@ -0,0 +1,21 @@
+/*const personer = [
+  { fornamn: "Anna", efternamn: "Andersson", stad: "Stockholm" },
+  { fornamn: "Erik", efternamn: "Eriksson", stad: "Göteborg" },
+  { fornamn: "Sara", efternamn: "Svensson", stad: "Malmö" },
+  { fornamn: "Lars", efternamn: "Larsson", stad: "Stockholm" },
+  { fornamn: "Emma", efternamn: "Ek", stad: "Göteborg" },
+  { fornamn: "Nils", efternamn: "Nilsson", stad: "Malmö" },
+  { fornamn: "Lisa", efternamn: "Lindberg", stad: "Stockholm" },
+  { fornamn: "Johan", efternamn: "Johansson", stad: "Göteborg" },
+  { fornamn: "Maria", efternamn: "Månsson", stad: "Malmö" },
+  { fornamn: "Per", efternamn: "Persson", stad: "Stockholm" }
+];
+
+const vipStatus = personer.map(person => ({
+    fullName: person.fornamn + " " + person.efternamn,
+    vip: person.efternamn[0] === "A" || person.efternamn[0] === "E"
+}));
+
+console.log("In-värde: Array med alla 10 personer:", personer);
+console.log("Ut-värde: Array med samma personer men med vip: true/false ", vipStatus);
+*/
